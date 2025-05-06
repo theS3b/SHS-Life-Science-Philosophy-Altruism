8257693e6d458ca4cf7f287dd1c072bccdf3ffba
RomainLogean <rom1logean@gmail.com>
2025-05-07 01:16:55 +0200

    fix donations

--- grid_simulation/cuda_env_project/cuda_square_simulation.py
+++ grid_simulation/cuda_env_project/cuda_square_simulation.py
@@ -156,9 +156,14 @@ class SquareSimulation:
         one_hot = torch.nn.functional.one_hot(argmax_pop, num_classes=P) \
                     .permute(0,3,1,2).bool()                   # (B,P,H,W)
 
-        # The actual donation are the contribution to both celected empty and non-empty cells
+        # The actual donation are the contribution to both selected empty and non-empty cells
         actual_donnations = torch.where(empty_mask & one_hot, contrib, 0.0)  # (B,P,H,W)
-        actual_donnations += torch.where(~empty_mask, contrib, 0.0)  # (B,P,H,W)
+
+        _, argmax_pop = self.grid.max(dim=1) 
+        population_ont_hot = torch.nn.functional.one_hot(argmax_pop, num_classes=P) \
+                    .permute(0,3,1,2).bool()
+        actual_donnations += torch.where(~empty_mask & population_ont_hot, contrib, 0.0)  # (B,P,H,W)
+
 
         new_grid = self.grid + actual_donnations
 
